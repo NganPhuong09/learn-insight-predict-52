e168528ce2e71dc50877d5c047dc8fe2b16d57f4
Phương Ngân <169058706+NganPhuong09@users.noreply.github.com>
2025-05-18 11:39:54 +0700

    Update ModelEvaluation.tsx

--- src/pages/ModelEvaluation.tsx
+++ src/pages/ModelEvaluation.tsx
@@ -1,6 +1,5 @@
 
 import React, { useState } from "react";
-import Navbar from "@/components/Navbar";
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
